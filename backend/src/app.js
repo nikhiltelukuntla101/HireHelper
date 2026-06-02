5a09ccb07c1f874be6ba9713ed1d2380f2fcb611
nikhiltelukuntla101 <nikhiltelukuntla999@gmail.com>
2026-06-02 21:54:06 +0530

    implemented Global ErrorHandler

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
+import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
+app.use(errorHandler);
 
 app.post("/test", (req, res) => {
   res.json({
--- backend/src/utils/AppError.js
+++ backend/src/utils/AppError.js
@@ -3,6 +3,7 @@ class AppError extends Error {
     super(message);
 
     this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
